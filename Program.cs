c02bcc0f16aa1a9eaa23c48d018ffe5647638297
Donald189 <donaldkarey@gmail.com>
2022-02-27 23:58:13 +0530

    Donald Address Book Edit

--- Program.cs
+++ Program.cs
@@ -8,14 +8,14 @@ namespace AddressBookSystem
         {
             Console.WriteLine("Welcome to Address Book \n");
 
-            //AddressBookMain addressBookMain = new AddressBookMain();
-            //addressBookMain.AddressBook();
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.AddressBook();
 
-            AddUsingConsole addUsingConsole = new AddUsingConsole();
-            addUsingConsole.readUserDetails();
+            //AddUsingConsole addUsingConsole = new AddUsingConsole();
+            //addUsingConsole.readUserDetails();
 
-            //EditPersonDetails editPersonDetails = new EditPersonDetails();
-            //editPersonDetails.editUserDetails();
+            EditPersonDetails editPersonDetails = new EditPersonDetails();
+            editPersonDetails.editUserDetails();
 
         }
     }
